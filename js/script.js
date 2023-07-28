9c29125c04a5239caab8d208da6d8cf76baa8dbe
rosclaritha <rosclaritha@gmail.com>
2023-07-28 22:59:56 +0800

    merge

--- js/script.js
+++ js/script.js
@@ -1232,4 +1232,29 @@
 			}
 		}
 	});
+
+	// logo slider
+	$(function(){
+		$('.customer-logos').slick({
+			slidesToShow: 5,
+			slidesToScroll: 1,
+			autoplay: true,
+			autoplaySpeed: 1500,
+			arrows: false,
+			dots: false,
+			pauseOnHover: false,
+			responsive: [{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 3
+				}
+			}, {
+				breakpoint: 520,
+				settings: {
+					slidesToShow: 1
+				}
+			}]
+		});
+	});
 }());
+
